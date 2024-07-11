af32ed6b45e698117e4b751bce3111fe707281f4
Carlos Carbajal <cmcarbajal@gmail.com>
2024-07-11 00:06:42 -0500

    update code using terra package

--- code_clhs_update.R
+++ code_clhs_update.R
@@ -5,42 +5,43 @@ library(sp)
 library(clhs)
 
 # List of raster file names
-files_raster <- c("Profile curvature_mantaro.tif", "Plan Curvature_mantaro.tif", "altura_standar_mantaro.tif", 
-                  "Aspect_mantaro.tif",  "nasadem_altomantaro.tif", "prof_valle_mantaro.tif", "Slope_mantaro.tif", 
-                  "TSAVI_mantaro.tif", "TPI_mantaro.tif", "twi_mantaro.tif", "tmed_mantaro_down.tif", 
-                  "lst_2021_mean_alto_mantaro_geo.tif", "Costo_acumulado_mantaro.tif")
+files_raster <- c("Aspect_mantaro.tif",  "Costo_acumulado_mantaro.tif", "nasadem_altomantaro.tif", 
+                "Slope_mantaro.tif", "tmed_mantaro_down.tif", "TPI_mantaro.tif", "TSAVI_mantaro.tif")
 
-# Create a stack of rasters directly within the loop
-r.stack <- terra::rast(lapply(files_raster, function(archivo) rast(paste("data/", archivo, sep = ""))))
+# Create a stack of rasters
+r.stack <- rast(paste0("data/", files_raster))
 
 # Assign names to the layers of the stack
-names(r.stack) <- c('curvpr', 'curvpl', 'hstand', 'aspect', 'dem', 'profund', 'slope', 'tavi', 'tpi', 'twi', 'tepm', 'lst', 'cost')
-
+names(r.stack) <- c('aspect', 'cost', 'dem', 'slope', 'tmed', 'tpi', 'tsavi')
 
 # Create a regular grid of points on top of our raster stack
 set.seed(5)
-s <- terra::init(r.stack, fun= runif)
+s <- init(r.stack, fun= runif)
 
 # Filter out the points with missing values in the 'cost' attribute
 s <- s[!is.na(s$cost), ]
 
-# Convert the matrix to a spatial object
-s_sf <- st_as_sf(s, coords = c("x", "y"))
+# Extract coordinates from the raster stack
+coords <- xyFromCell(r.stack, 1:ncell(r.stack))
 
-# Convert the spatial object to a SpatialPointsDataFrame
-s_spdf <- as(s_sf, "Spatial")
+# Combine coordinates and values
+s_df <- cbind(coords, s)
 
-# Now you have s_spdf as a SpatialPointsDataFrame
+# Combine coordinates and values (as data.frame)
+s_df <- data.frame(coords, s) # Convert to data.frame
+
+# Create a spatial points data frame
+s_spdf <- SpatialPointsDataFrame(coords = s_df[, 1:2], data = s_df[, 3:ncol(s_df)])
 
 # The cost surface is activated through the 'cost' argument
-s.clhs <- clhs(s, size = 120, progress = FALSE, iter = 2000, cost = 'cost', simple = FALSE)
+s.clhs <- clhs(s_spdf, size = 120, progress = FALSE, iter = 2000, cost = 'cost', simple = FALSE)
 
 # Extract the indices
 subset.idx <- s.clhs$index_samples
 
 # Visualize the DEM and cost layers, with the selected points marked in red
 terra::plot(r.stack$dem, axes=FALSE)
-terra::contour(r.stack$cost, nlevels=10, col='black', add=TRUE)
+terra::contour(r.stack$cost, nlevels=10, col='#1a1818', add=TRUE)
 points(s[subset.idx, ], col = 'red', pch=21)
 
 # Save the selected points as an ESRI Shapefile named 'clhs_mantaro_points' in the 'outputs_mantaro' directory
