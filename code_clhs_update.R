b10ca3ea65434c5532d510a30264a0a3b53afa7f
Carlos Carbajal <cmcarbajal@gmail.com>
2024-07-14 11:29:30 -0500

    update code reducing area

--- code_clhs_update.R
+++ code_clhs_update.R
@@ -7,7 +7,7 @@ library(raster)
 
 # List of raster file names
 files_raster <- c("aspect_basin.tif",  "cost_basin.tif", "nasadem_basin.tif", 
-                "slope_basin.tif", "tmed_basin.tif", "tpi_basintif", "tsavi_basin.tif")
+                "slope_basin.tif", "tmed_basin.tif", "tpi_basin.tif", "tsavi_basin.tif")
 
 # Create a stack of rasters
 r.stack <- rast(paste0("data/", files_raster))
@@ -17,7 +17,7 @@ names(r.stack) <- c('aspect', 'cost', 'dem', 'slope', 'tmed', 'tpi', 'tsavi')
 
 # Create a regular grid of points on top of our raster stack
 set.seed(5)
-s <- spatSample(r.stack, size= 12000, method = "regular", xy = TRUE)
+s <- spatSample(r.stack, size= 1000, method = "regular", xy = TRUE)
 
 # Filter out the points with missing values in the 'cost' attribute
 s <- s[!is.na(s$cost), ]
@@ -26,18 +26,18 @@ s <- s[!is.na(s$cost), ]
 s_spdf <- SpatialPointsDataFrame(coords = s[, 1:2], data = s[, 3:ncol(s)])
 
 # The cost surface is activated through the 'cost' argument
-s.clhs <- clhs(s_spdf, size = 120, progress = FALSE, iter = 2000, cost = 'cost', simple = FALSE)
+s.clhs <- clhs(s_spdf, size = 60, progress = FALSE, iter = 2000, cost = 'cost', simple = FALSE)
 
 # Extract the indices
 subset.idx <- s.clhs$index_samples
 
 # Visualize the DEM and cost layers, with the selected points marked in red
-terra::plot(r.stack$dem, axes=FALSE)
-terra::contour(r.stack$cost, nlevels=10, col='#1a1818', add=TRUE)
-points(s[subset.idx, ], col = 'red', pch=21)
+terra::plot(r.stack$dem, axes=TRUE, legend=TRUE)
+terra::contour(r.stack$cost, nlevels=10, col='#464343', add=TRUE)
+points(s[subset.idx, ], col = '#FF00C8', pch=16, cex = 1.5)
 
 # Save the selected points as an ESRI Shapefile named 'clhs_mantaro_points' in the 'outputs_mantaro' directory
-st_write(s[subset.idx, ], "outputs/clhs_mantaro_points.csv")
+st_write(s[subset.idx, ], "outputs/clhs_points.csv")
 
 
 ### Similarity Buffer analysis ###
@@ -49,7 +49,7 @@ df_raster <- as.data.frame(r.stack, xy = TRUE)
 coordinates(df_raster) <- c("x", "y")
 
 # Load the raster layer
-rast_layer <- raster("data/nasadem_altomantaro.tif")
+rast_layer <- raster("data/nasadem_basin.tif")
 
 # Create a raster template
 r <- raster(extent(rast_layer), res = 0.0002777778, crs = crs(rast_layer)) 
@@ -66,7 +66,7 @@ for (i in 1:ncol(df_raster@data)) {
 }
 
 # Calculate the Gower similarity index between the raster stack and the sampled data from the CLHS analysis, with a buffer of 250
-gw_cuenca <- similarity_buffer(r.stack, s.clhs$sampled_data, buffer = 250)
+gw_cuenca <- similarity_buffer(r.stack, s.clhs$sampled_data, buffer = 500)
 
 # Save the buffer results as a GeoTIFF named 'list_buffer.tif' in the 'outputs_mantaro' directory
-terra::writeRaster(gw_cuenca, "outputs/list_buffer.tif")
+terra::writeRaster(gw_cuenca, "outputs/grids_buffer.tif")
